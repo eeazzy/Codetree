b69f849fc115cdfb4bf0ec8b92ee6ae139268319
김현서 <143472229+eeazzy@users.noreply.github.com>
2025-03-07 18:04:21 +0900

    Title: 실수 받아 그대로 출력, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250307/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double num = Double.parseDouble(sc.next());
+        System.out.println(String.format("%.2f", num));
+    }
+}
